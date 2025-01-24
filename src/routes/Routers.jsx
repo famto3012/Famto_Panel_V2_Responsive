4eef4da85463523c189150a86a17bdd8555b40d8
Akhilbabu456 <akhilbeliever001@gmail.com>
2025-01-24 12:41:25 +0530

    Updates

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,3 +1,139 @@
+// import {
+//   Route,
+//   Routes,
+//   useNavigate,
+//   useLocation,
+//   Navigate,
+// } from "react-router-dom";
+// import { Suspense, lazy, useContext, useEffect, useState } from "react";
+
+// import ProtectedRoute from "./ProtectedRoutes";
+
+// import Loader from "@/components/others/Loader";
+// import AuthContext from "@/context/AuthContext";
+// import Maintenance from "@/screens/other/Maintenance";
+// import WhatsappRoutes from "./WhatsappRoute";
+// import { fetchAllowedRoutes } from "@/hooks/manager/useManager";
+// import { allowedMerchantRouteOptions } from "@/utils/defaultData";
+// import { useQuery } from "@tanstack/react-query";
+
+// // Lazy load each route component
+// const AuthRoutes = lazy(() => import("./AuthRoute"));
+// const HomeRoutes = lazy(() => import("./HomeRoute"));
+// const OrderRoutes = lazy(() => import("./OrderRoute"));
+// const MerchantRoutes = lazy(() => import("./MerchantRoute"));
+// const ProductRoutes = lazy(() => import("./ProductRoute"));
+// const CustomerRoutes = lazy(() => import("./CustomerRoute"));
+// const AgentRoutes = lazy(() => import("./AgentRoute"));
+// const DeliveryRoutes = lazy(() => import("./DeliveryRoute"));
+// const CommAndSubsRoutes = lazy(() => import("./CommAndSubsRoute"));
+// const MarketingRoutes = lazy(() => import("./MarketingRoute"));
+// const NotificationRoutes = lazy(() => import("./NotificationRoute"));
+// const ConfigureRoutes = lazy(() => import("./ConfigureRoute"));
+// const CustomizeRoutes = lazy(() => import("./CustomizeRoute"));
+// const AccountRoutes = lazy(() => import("./AccountRoute"));
+
+// const Routers = () => {
+//   const { token, role } = useContext(AuthContext);
+//   const navigate = useNavigate();
+//   const location = useLocation();
+//   const [allowedRoutes, setAllowedRoutes] = useState([]);
+
+//   useEffect(() => {
+//     if (!token && !location.pathname.startsWith("/auth")) {
+//       navigate("/auth/sign-in");
+//     } else if (token && location.pathname.startsWith("/auth")) {
+//       navigate("/home");
+//     }
+
+//     // navigate("/maintenance");
+//   }, [token, location.pathname, navigate]);
+
+//   const { data } = useQuery({
+//     queryKey: ["allowed-routes"],
+//     queryFn: () => fetchAllowedRoutes(navigate),
+//     enabled: (token && role !== "Admin") || role !== "Merchant",
+//   });
+
+//   useEffect(() => {
+//     if (role === "Merchant") {
+//       setAllowedRoutes(allowedMerchantRouteOptions);
+//     } else if (role !== "Admin" && role !== "Merchant" && data) {
+//       console.log("Routes", data);
+//       setAllowedRoutes(data);
+//     }
+//   }, [data]);
+
+//   return (
+//     <Suspense fallback={<Loader />}>
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/home" replace />} />
+//         <Route path="/auth/*" element={<AuthRoutes />} />
+//         <Route path="/home/*" element={<HomeRoutes />} />
+
+//         <Route path="/order/*" element={<OrderRoutes />} />
+
+//         <Route
+//           path="/merchant/*"
+//           element={
+//             <ProtectedRoute requiredRole="Admin">
+//               <MerchantRoutes />
+//             </ProtectedRoute>
+//           }
+//         />
+
+//         <Route path="/product/*" element={<ProductRoutes />} />
+
+//         <Route path="/customer/*" element={<CustomerRoutes />} />
+
+//         <Route
+//           path="/agent/*"
+//           element={
+//             <ProtectedRoute>
+//               <AgentRoutes />
+//             </ProtectedRoute>
+//           }
+//         />
+
+//         <Route
+//           path="/delivery-management/*"
+//           element={
+//             <ProtectedRoute>
+//               <DeliveryRoutes />
+//             </ProtectedRoute>
+//           }
+//         />
+
+//         <Route path="/comm-and-subs/*" element={<CommAndSubsRoutes />} />
+//         <Route path="/chat/*" element={<WhatsappRoutes />} />
+//         <Route path="/marketing/*" element={<MarketingRoutes />} />
+//         <Route path="/notification/*" element={<NotificationRoutes />} />
+//         <Route
+//           path="/configure/*"
+//           element={
+//             <ProtectedRoute requiredRole="Admin">
+//               <ConfigureRoutes />
+//             </ProtectedRoute>
+//           }
+//         />
+//         <Route
+//           path="/customize/*"
+//           element={
+//             <ProtectedRoute requiredRole="Admin">
+//               <CustomizeRoutes />
+//             </ProtectedRoute>
+//           }
+//         />
+//         <Route path="/account/*" element={<AccountRoutes />} />
+//         <Route path="/maintenance" element={<Maintenance />} />
+//         <Route path="*" element={<Navigate to="/home" replace />} />
+//       </Routes>
+//     </Suspense>
+//   );
+// };
+
+// export default Routers;
+
 import {
   Route,
   Routes,
@@ -52,91 +188,66 @@ const Routers = () => {
   const { data } = useQuery({
     queryKey: ["allowed-routes"],
     queryFn: () => fetchAllowedRoutes(navigate),
-    enabled: token && role === "Manager" ? true : false,
+    enabled: (token && role !== "Admin") || role !== "Merchant",
   });
 
   useEffect(() => {
     if (role === "Merchant") {
       setAllowedRoutes(allowedMerchantRouteOptions);
-    } else if (role === "Manager" && data) {
+    } else if (role !== "Admin" && role !== "Merchant" && data) {
       setAllowedRoutes(data);
     }
-  }, [data]);
+  }, [data, role]);
 
-  const canAccessRoute = (route) => {
-    console.log(route, role);
+  const allRoutes = [
+    { path: "/home/*", element: <HomeRoutes /> },
+    { path: "/order/*", element: <OrderRoutes /> },
+    { path: "/merchant/*", element: <MerchantRoutes /> },
+    { path: "/product/*", element: <ProductRoutes /> },
+    { path: "/customer/*", element: <CustomerRoutes /> },
+    { path: "/agent/*", element: <AgentRoutes /> },
+    { path: "/delivery-management/*", element: <DeliveryRoutes /> },
+    { path: "/comm-and-subs/*", element: <CommAndSubsRoutes /> },
+    { path: "/chat/*", element: <WhatsappRoutes /> },
+    { path: "/marketing/*", element: <MarketingRoutes /> },
+    { path: "/notification/*", element: <NotificationRoutes /> },
+    { path: "/configure/*", element: <ConfigureRoutes /> },
+    { path: "/customize/*", element: <CustomizeRoutes /> },
+    { path: "/account/*", element: <AccountRoutes /> },
+  ];
 
-    if (role === "Admin") {
-      return true;
-    } else {
-      allowedRoutes.some((r) => r.route === route);
-    }
-  };
+  // Filter routes based on allowedRoutes
+  const filteredRoutes =
+    role === "Admin"
+      ? allRoutes // Admin gets access to all routes
+      : allRoutes.filter((route) =>
+          allowedRoutes.some((allowed) => {
+            route.path.startsWith(allowed.value);
+          })
+        );
 
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/home/*" element={<HomeRoutes />} />
-        {canAccessRoute("/order") && (
-          <Route path="/order/*" element={<OrderRoutes />} />
+
+        {filteredRoutes.map((route, index) =>
+          route.protected ? (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <ProtectedRoute requiredRole="Admin">
+                  {route.element}
+                </ProtectedRoute>
+              }
+            />
+          ) : (
+            <Route key={index} path={route.path} element={route.element} />
+          )
         )}
-        {canAccessRoute("/merchant") && (
-          <Route
-            path="/merchant/*"
-            element={
-              <ProtectedRoute requiredRole="Admin">
-                <MerchantRoutes />
-              </ProtectedRoute>
-            }
-          />
-        )}
-        {canAccessRoute("/product") && (
-          <Route path="/product/*" element={<ProductRoutes />} />
-        )}
-        <Route path="/customer/*" element={<CustomerRoutes />} />
-        {canAccessRoute("/agent") && (
-          <Route
-            path="/agent/*"
-            element={
-              <ProtectedRoute>
-                <AgentRoutes />
-              </ProtectedRoute>
-            }
-          />
-        )}
-        {canAccessRoute("/delivery-management") && (
-          <Route
-            path="/delivery-management/*"
-            element={
-              <ProtectedRoute>
-                <DeliveryRoutes />
-              </ProtectedRoute>
-            }
-          />
-        )}
-        <Route path="/comm-and-subs/*" element={<CommAndSubsRoutes />} />
-        <Route path="/chat/*" element={<WhatsappRoutes />} />
-        <Route path="/marketing/*" element={<MarketingRoutes />} />
-        <Route path="/notification/*" element={<NotificationRoutes />} />
-        <Route
-          path="/configure/*"
-          element={
-            <ProtectedRoute requiredRole="Admin">
-              <ConfigureRoutes />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/customize/*"
-          element={
-            <ProtectedRoute requiredRole="Admin">
-              <CustomizeRoutes />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/account/*" element={<AccountRoutes />} />
+
         <Route path="/maintenance" element={<Maintenance />} />
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
